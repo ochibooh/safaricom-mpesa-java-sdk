8b00c3bbcdeb512087181faf6454e11362ebe39a
ochibooh <9phelix1@gmail.com>
2020-01-12 19:52:09 +0300

    Account Balance
    Test Account Balance Request. Add Timeout and Result Callback endpoints

---
+++ mpesa/src/main/java/com/ochibooh/safaricom/mpesa/model/request/MpesaTransactionStatusRequest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2020
+ *     Phelix Ochieng(Ochibooh)
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ochibooh.safaricom.mpesa.model.request;
+
+import com.google.gson.annotations.SerializedName;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+@ToString
+@Setter
+@Getter
+@Builder
+public class MpesaTransactionStatusRequest implements Serializable {
+    @SerializedName("Initiator")
+    private String initiator;
+
+    @SerializedName("SecurityCredential")
+    private String credential;
+
+    @SerializedName("CommandID")
+    private String commandId;
+
+    @SerializedName("TransactionID")
+    private String transactionId;
+
+    @SerializedName("PartyA")
+    private String partyA;
+
+    @SerializedName("IdentifierType")
+    private Integer identifierType;
+
+    @SerializedName("Remarks")
+    private String description;
+
+    @SerializedName("Occasion")
+    private String occasion;
+
+    @SerializedName("QueueTimeOutURL")
+    private String queueTimeoutUrl;
+
+    @SerializedName("ResultURL")
+    private String resultUrl;
+}
